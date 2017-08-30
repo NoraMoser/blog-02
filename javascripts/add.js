f08550cfb89dbf61897d44a6f67743ed7b32fee9
Nora Moser <noramoser@Noras-MacBook-Pro.local>
2017-08-30 16:20:30 -0500

    finished part 1

--- javascripts/add.js
+++ javascripts/add.js
@@ -9,4 +9,42 @@ addLink.addEventListener("click", function() {
   addView.classList.add("visible");
   addView.classList.remove("hidden");
 
+  var inputButton = document.getElementById("addButton");
+  
+  inputButton.addEventListener("click", function(){
+    
+    displayNewBlogs();
+    displayOriginalBlogs();
+    
+  });
 });
+
+
+
+function displayNewBlogs() {
+  var blogTitleInput = document.getElementById("title");
+  var blogDateInput = document.getElementById("date");
+  var blogAuthorInput = document.getElementById("author");
+  var blogEntryInput = document.getElementById("entry");
+  var blogKeywordInput = document.getElementById("keyword");
+  
+  blogs1.unshift({title: blogTitleInput.value, date: blogDateInput.value, author: blogAuthorInput.value, entry: blogEntryInput, keywords: blogKeywordInput});
+  
+}
+
+function displayOriginalBlogs() {
+  contentArea2.innerHTML = "";
+  for (let j = 0; j < blogs1.length; j++) {
+  
+
+    contentArea2.innerHTML += `<div class="blog-one">
+                                <h2>${blogs1[j].title}</h2>
+                                <h4 class="bloglist">${blogs1[j].date}</h4>
+                                <h4 class="bloglist">${blogs1[j].author}</h4>
+                                <h4 class="bloglist">${blogs1[j].entry}</h4>
+                                <p class="bloglist">${blogs1[j].keywords}</p>
+                                </div>`;
+                            
+  }
+ 
+  }
